0b0edf25e4b0dbf22a94b614c335bfc0dae371b3
Yar Night <yaroslav239@gmail.com>
2023-05-12 18:20:07 +0300

    feat: add function createBody

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -1,18 +1,80 @@
-const field = document.getElementById("field");
+function createBody(){
+    const wrapper = document.createElement("DIV");
+    wrapper.classList.add("wrapper");
 
-for(let i = 0; i < 100; i++) {
-    const btn = document.createElement("BUTTON");
-    btn.classList.add("cell-small");
-    field.append(btn); 
+    const header = document.createElement("HEADER");
+    header.classList.add("header");
+    const h1 = document.createElement("H1");
+    h1.classList.add("title"); 
+    h1.textContent = "Welcome to Minesweeper"; 
+    header.append(h1); 
+
+    const main = document.createElement("MAIN");
+    main.classList.add("main"); 
+    const mainDescription = document.createElement("H2");
+    mainDescription.classList.add("footer-text");
+    mainDescription.textContent = "select level of the game 10/10 20/20 30/30";
+    const levels = document.createElement("DIV");
+    levels.classList.add("level");
+    const easyButton = document.createElement("BUTTON"); 
+    easyButton.classList.add("btn");
+    easyButton.textContent = "Easy";
+    const mediumButton = document.createElement("BUTTON"); 
+    mediumButton.classList.add("btn");
+    mediumButton.textContent = "Medium";
+    const hardButton = document.createElement("BUTTON"); 
+    hardButton.classList.add("btn");
+    hardButton.textContent = "Hard";
+    levels.append(easyButton);
+    levels.append(mediumButton);
+    levels.append(hardButton);
+    main.append(mainDescription);
+    main.append(levels);
+
+    const footer = document.createElement("FOOTER");
+    footer.classList.add("footer"); 
+    const footerContent = document.createElement("DIV");
+    footerContent.classList.add("footer-content");
+    const time = document.createElement("H2");
+    time.classList.add("footer-text");
+    time.textContent = "Time: 00sec";
+    const bombs = document.createElement("H2");
+    bombs.classList.add("footer-text");
+    bombs.textContent = "Bombs left: 30";
+    const footerButton = document.createElement("BUTTON"); 
+    footerButton.classList.add("btn");
+    footerButton.classList.add("text_dark");
+    footerButton.classList.add("bg_light");
+    footerButton.textContent = "Restart"; 
+    footerContent.append(time);
+    footerContent.append(bombs);
+    footer.append(footerContent);
+    footer.append(footerButton);
+
+    wrapper.append(header);
+    wrapper.append(main);
+    wrapper.append(footer);
+    document.body.prepend(wrapper); 
 }
 
-let width = 10; 
-let height = 10; 
-let bombs = 10; 
-let cellsNumber = width*height; 
-let bombIndexes = [...Array(cellsNumber).keys()].sort(() => Math.random() - 0.5).slice(0, bombs);
-let cells = [...field.children];
-let closedCount = cellsNumber; 
+createBody()
+
+
+// const field = document.getElementById("field");
+
+// for(let i = 0; i < 100; i++) {
+//     const btn = document.createElement("BUTTON");
+//     btn.classList.add("cell-small");
+//     field.append(btn); 
+// }
+
+// let width = 10; 
+// let height = 10; 
+// let bombs = 10; 
+// let cellsNumber = width*height; 
+// let bombIndexes = [...Array(cellsNumber).keys()].sort(() => Math.random() - 0.5).slice(0, bombs);
+// let cells = [...field.children];
+// let closedCount = cellsNumber; 
 
 function isValid(row, column){
     return row >= 0 && row < height && column >= 0 && column < width; 
@@ -81,4 +143,4 @@ function handler(event) {
     open(row, column);
 }
 
-field.addEventListener("click", handler);
+// field.addEventListener("click", handler);
